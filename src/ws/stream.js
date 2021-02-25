7032fe244b160b263856b93bea4b76008a69d8b3
Zack <aminuzack7@gmail.com>
2021-02-25 20:37:03 +0100

    fix isuues

--- src/ws/stream.js
+++ src/ws/stream.js
@@ -2,12 +2,6 @@ const users = {};
 const rooms = {};
 const stream = ( socket ) => {
     socket.on( 'subscribe', ( data ) => {
-
-        // const isRooMAvail = rooms[data.room.split("_")[0]];
-        // if(isRooMAvail) {
-        //     return socket.emit( 'roomExist', {message: `The room "${isRooMAvail}" already exist.` } );
-        // }
-
         //subscribe/join a room
         socket.join( data.room );
         socket.join( data.socketId );
@@ -20,25 +14,6 @@ const stream = ( socket ) => {
             socket.to( data.room ).emit( 'new user', { socketId: data.socketId } );
         }
     } );
-
-    // socket.on( 'join', ( data ) => {
-
-    //     const isRooMAvail = rooms[data.room.split("_")[0]];
-    //     if(!isRooMAvail) {
-    //         return socket.emit( 'roomDoesNotExist', {  message: 'Meeting has ended or the link is invalid.' } );
-    //     }
-    //     //subscribe/join a room
-    //     socket.join( data.room );
-    //     socket.join( data.socketId );
-
-    //     users[data.socketId] = `${data.user}*${data.room}`;;
-
-    //     //Inform other members in the room of new user's arrival
-    //     if ( socket.adapter.rooms[data.room].length > 1 ) {
-    //         socket.to( data.room ).emit( 'new user', { socketId: data.socketId } );
-    //     }
-    // } );
-
 
     socket.on( 'newUserStart', ( data ) => {
         socket.to( data.to ).emit( 'newUserStart', { sender: data.sender } );
@@ -59,18 +34,6 @@ const stream = ( socket ) => {
         socket.to( data.room ).emit( 'chat', { sender: data.sender, msg: data.msg } );
     } );
 
-    socket.on('disconnect', () => {
-        socket.id = socket.id.split('#')[1];
-        let userName = users[socket.id];
-        if(userName){
-            delete rooms[userName.split('*')[1].split('_')[0]];
-        }
-        delete users[socket.id];
-        console.log(userName);
-        if(userName) {
-            socket.to(userName.split('*')[1]).emit( 'userLeft', { name: userName.split('*')[0] })
-        }
-    })
 };
 
 module.exports = stream;
